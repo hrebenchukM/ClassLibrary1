7704ba8ba35c4970284b9d3735f91181763c1213
Oleg-code-git <mices98589@deusa7.com>
2025-05-12 01:29:34 +0300

    Изменние 3 : Добавлен SaveClass

---
+++ ClassLibrary2/SaveClass.cs
@@ -0,0 +1,13 @@
+﻿
+using ClassLibrary1;
+
+namespace ClassLibrary2
+{
+    public class SaveClass : InterfaceSave
+    {
+        public void SaveText(string s)
+        {
+         
+        }
+    }
+}
