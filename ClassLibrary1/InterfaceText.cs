b2e91a11ae5df077aae4131616974fd39aaff54e
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2025-05-12 01:05:51 +0300

    Измнение 4: Добавлен метод SelectText

--- ClassLibrary1/InterfaceText.cs
+++ ClassLibrary1/InterfaceText.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ClassLibrary1
 {
-    internal class InterfaceText
+    public interface InterfaceText
     {
+        public Font SelectText();
     }
 }
