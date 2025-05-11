196c8cc5d964fc6d9c3c0c0ac4c21c7df33dd092
Maryprog <marya101204@gmail.com>
2025-05-12 02:01:47 +0300

    Изменение 1 : Program

---
+++ DLL/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace DLL
+{
+    internal static class Program
+    {
+    
+        static void Main()
+        {
+        
+        }
+    }
+}
